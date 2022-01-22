0b0995f492f5249154dae6b4e90b0c5dc74dc604
Mubor <muhatasov22@ukr.net>
2022-01-22 23:32:50 +0200

    fixed

--- js/WeatherSDK.js
+++ js/WeatherSDK.js
@@ -1,6 +1,13 @@
 function Weather (city) {
     this.city = city;
     this.init = fetch("http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no")
-        .then(response => response.json());
+        .then( response => response.json() )
+        .then( data => {
+            this.temp = data.current.temp_c;
+            this.pressure = data.current.pressure_mb;
+            this.wind = data.current.wind_kph;
+            this.humidity = data.current.humidity;
+            return this;
+        });
 }
 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 let kyiv = new Weather("Kyiv");
-kyiv.init.then((data) => {
-    document.body.innerHTML = data.current.temp_c;
+let a = kyiv.init.then( data => {
+    document.body.innerHTML = `${data.temp}<br>${data.pressure}<br>${data.wind}<br>${data.humidity}`;
 });
