f431d7827407d2833995a2260dad03c61d8c209b
Mubor <muhatasov22@ukr.net>
2022-01-27 05:03:26 +0200

    added URL variables

--- js/WeatherSDK.js
+++ js/WeatherSDK.js
@@ -1,6 +1,5 @@
-function Weather (city) {
-    this.city = city;
-    this.init = fetch("http://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no")
+function Weather (city, token) {
+    this.init = fetch(`http://api.weatherapi.com/v1/current.json?key=${token}&q=${city}&aqi=no`)
         .then( response => response.json() )
         .then( data => {
             this.temp = data.current.temp_c;
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let kyiv = new Weather("Kyiv");
+let kyiv = new Weather("Kiev", "b677082ac19f45aca07212645220401");
 kyiv.init.then( data => {
     document.body.innerHTML = `${data.temp}<br>${data.pressure}<br>${data.wind}<br>${data.humidity}`;
 });
